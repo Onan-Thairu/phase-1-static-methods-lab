915db3993816ccc814020972c0dfbdd22fed5441
OnanThairu <thairuonan@gmail.com>
2022-07-03 10:12:34 +0300

    commit tree 70e39f8af04856b4cab26563cccedba9e2966c6c

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 class Formatter {
-    static capitalize(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1)
-    }
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-']+/g, "")
+  }
+
+  static titleize(string) {
+    return string.capitalize
+  }
 }
